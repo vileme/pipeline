67500ab30637f3ba1fe27841d54d8800357e3fb9
vileme <wickdtarazed@gmail.com>
2021-05-09 08:01:15 +0400

    added logging time

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ def get_split(train_test_split_file='./data/train_test_id.pickle'):
 def main():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    server = False
+    server = True
     path_default = "./data" if server else "e:/diploma"
     arg('--jaccard-weight', type=float, default=1)
     arg('--t', type=float, default=0.07)
@@ -49,9 +49,9 @@ def main():
     arg('--pretrain-image-path', type=str, default= f'{path_default}/ham10000/', help='train test split file path')
     arg('--pretrain-mask-image-path', type=str, default=f'{path_default}/ham_clusters_20/lab/20/', help="images path for pretraining")
     arg('--image-path', type=str, default=f'{path_default}/task2_h5/', help="h5 images path for training")
-    arg('--batch-size', type=int, default=8, help="n batches")
-    arg('--workers', type=int, default=1, help="n workers")
-    arg('--cuda-driver', type=int, default=2, help="cuda driver")
+    arg('--batch-size', type=int, default=5, help="n batches")
+    arg('--workers', type=int, default=4, help="n workers")
+    arg('--cuda-driver', type=int, default=1, help="cuda driver")
     arg('--lr', type=float, default=0.001, help="lr")
     args = parser.parse_args()
 
@@ -67,7 +67,7 @@ def main():
     num_classes = 5
     args.num_classes = 5
     model = UNet16(num_classes= num_classes, pretrained="vgg")
-    model = nn.DataParallel(model, device_ids=[args.cuda_driver])
+    model = nn.DataParallel(model, device_ids=[args.cuda_driver, 0, 2, 3])
     model.to(device)
 
 
@@ -84,6 +84,7 @@ def main():
     scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.8, patience=5, verbose=True)
     print(model)
     print('Start pretraining')
+    criterion = ContrastiveLoss(args.t, device)
     # wandb.watch(model)
     for epoch in range(epoch, args.pretrain_epochs + 1):
         model.train()
@@ -91,7 +92,6 @@ def main():
         losses = []
         for ind, (id, image_original, image_transformed, mask_original, mask_transformed) in enumerate(pretrain_loader):
             start_step = time.time()
-            criterion = ContrastiveLoss(args.t, device)
             #print(torch.cuda.memory_allocated(device)/ (1024 ** 2))
             #print(torch.cuda.memory_reserved(device)/(1024 ** 2))
             #print(torch.cuda.max_memory_allocated(device) / (1024 ** 2))
@@ -103,17 +103,23 @@ def main():
                 torch.cuda.ByteTensor if torch.cuda.is_available() else torch.ByteTensor)
             mask_transformed = mask_transformed.to(device).type(
                 torch.cuda.ByteTensor if torch.cuda.is_available() else torch.ByteTensor)
+            forward_start = time.time()
             original_result, _ = model(train_image_original)
             transformed_result, _ = model(train_image_transformed)
+            print(f"forward start :{time.time() - forward_start}")
             loss = (criterion(original_result, transformed_result, mask_original, mask_transformed))
             losses.append(loss.item())
             print(
                 f'epoch={epoch:3d},iter={ind:3d}, loss={loss.item():.4g}')
+            zero_grad_start = time.time()
             optimizer.zero_grad()
+            print(f"zero grad time:{time.time() - zero_grad_start}")
             start_backward = time.time()
             loss.backward()
             print(f"backward time:{time.time() - start_backward}")
+            start_optimizer = time.time()
             optimizer.step()
+            print(f"oprimizer time:{time.time() - start_optimizer}")
             print(f"step time:{time.time() - start_step}")
 
         avg_loss = np.mean(losses)
